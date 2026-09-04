8e2429785a3e7c69874e372d440678d63306f947
Shigeta Iida <119585832+shigety2020@users.noreply.github.com>
2026-09-04 12:31:08 +0900

    fix: restore masthead desk JPEG assemblers a-b

--- app/masthead/jpeg-a.ts
+++ app/masthead/jpeg-a.ts
@@ -1,1 +1,9 @@
-LOAD_FROM_FILE:/workspace/masthead-split/jpeg-a.ts
+import { a0 } from "./parts/a0";
+import { a1 } from "./parts/a1";
+import { a2 } from "./parts/a2";
+import { a3 } from "./parts/a3";
+import { a4 } from "./parts/a4";
+import { a5 } from "./parts/a5";
+import { a6 } from "./parts/a6";
+import { a7 } from "./parts/a7";
+export const a = a0+a1+a2+a3+a4+a5+a6+a7;
--- app/masthead/jpeg-b.ts
+++ app/masthead/jpeg-b.ts
@@ -1,1 +1,9 @@
-PLACEHOLDER_B
+import { b0 } from "./parts/b0";
+import { b1 } from "./parts/b1";
+import { b2 } from "./parts/b2";
+import { b3 } from "./parts/b3";
+import { b4 } from "./parts/b4";
+import { b5 } from "./parts/b5";
+import { b6 } from "./parts/b6";
+import { b7 } from "./parts/b7";
+export const b = b0+b1+b2+b3+b4+b5+b6+b7;

3f1c98387d3ffea243f6222d5e359157b5a1d1a7
Shigeta Iida <119585832+shigety2020@users.noreply.github.com>
2026-09-06 20:19:03 +0900

    fix: masthead jpeg assemblers for desk JPEG (no placeholders)

--- app/masthead/jpeg-d.ts
+++ app/masthead/jpeg-d.ts
@@ -1,1 +1,9 @@
-PLACEHOLDER_LOAD_FROM_FILE
+import { d0 } from "./parts/d0";
+import { d1 } from "./parts/d1";
+import { d2 } from "./parts/d2";
+import { d3 } from "./parts/d3";
+import { d4 } from "./parts/d4";
+import { d5 } from "./parts/d5";
+import { d6 } from "./parts/d6";
+import { d7 } from "./parts/d7";
+export const d = d0+d1+d2+d3+d4+d5+d6+d7;
